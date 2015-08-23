817e2d7c22366886be1882ffa51fe1f609cc95e7
ms <schmiddim@gmx.at>
2015-08-23 19:56:34 +0200

    first commit

---
+++ octoprint-status.php
@@ -0,0 +1,9 @@
+#!/usr/bin/php
+<?php
+
+/*
+http://docs.octoprint.org/en/master/api/job.html#retrieve-information-about-the-current-job
+http://192.168.0.105:5000/api/printer?history=true&limit=2&apikey=951657F1F5F34B23AF6419B3C3E75192
+http://192.168.0.105:5000/api/job?apikey=951657F1F5F34B23AF6419B3C3E75192
+
+*/
